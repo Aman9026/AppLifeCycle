aa498944c61f622f7036bbb76b5b4120e7103456
Aman9026 <amangupta884036@gmail.com>
2020-09-19 23:31:47 +0530

    added state tracker

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  AppLifecycleState _appLifecycleState;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      _appLifecycleState = state;
+      print("State of this app: $_appLifecycleState");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            _appLifecycleState.toString(),
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
